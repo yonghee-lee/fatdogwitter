dd8ef44ea5d6bc18111544778310e0a3396b683c
YongHee Lee <madhatter@YongHeeui-MacBook.local>
2022-07-18 21:30:20 +0900

    add navigation, login

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 //import './index.css';
-import App from './App';
+import App from 'components/App';
 //import reportWebVitals from './reportWebVitals';
-import firebase from "./firebase";
+import firebase from "fbase";
 console.log(firebase);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
